308ff918cc05a74ebc5d0ab61c9904b60e7194aa
Maruf-dev <maruf123erkinov@gmail.com>
2022-10-30 13:22:55 +0500

    kitobland version 1.5

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let dayNight = document.querySelector('.day_night');
+dayNight.onclick = function(){
+    dayNight.classList.toggle('active')
+}
+let menuBar = document.querySelector('.icon');
+menuBar.onclick = function(){
+    menuBar.classList.toggle('active')
+}
